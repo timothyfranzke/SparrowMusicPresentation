40e9833c1665f58f9c0656f0b1d0d5d130bbd0b9
=timothyfranzke <timothy@franzkecreative.com>
2016-02-20 11:12:48 -0600

    updated the css sliding for filters and artist create wizard

--- app/controllers/artistFormController.js
+++ app/controllers/artistFormController.js
@@ -1,5 +1,18 @@
 sprwApp.controller('artistFormController', function($scope, $state, $mdDialog, artistService, authServices){
     var userData = authServices.getUserData();
+    $scope.artistNameShow = true;
+    $scope.artistThemeShow = false;
+    $scope.artistImageSelectShow = false;
+    $scope.artistImageCropShow = false;
+    $scope.artistNoImageConfirmShow = true;
+    $scope.artistImageCropResultShow = false;
+
+    $scope.themes = [
+        {color: "Red", class:"sparrow-red"},
+        {color: "Blue", class:"sparrow-blue"},
+        {color: "Yellow", class:"sparrow-yellow"},
+        {color: "Purple", class:"sparrow-purple"},
+    ]
 
     if(userData.token === undefined || userData.userEmail === undefined) {
         $state.go("login");
